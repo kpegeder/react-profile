e6161b664b5d03f607d0621824fc8bb3edcf97ee
kpegeder <k.pegeder@gmail.com>
2020-12-04 17:42:22 -0800

    setting up components

---
+++ src/components/Nav/index.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Nav() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark">
+      <div>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link
+              to="/"
+              className={
+                window.location.pathname === "/" ||
+                window.location.pathname === "/home"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/"
+              className={
+                window.location.pathname === "/portfolio"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              Portfolio
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/"
+              className={
+                window.location.pathname === "/contact"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
