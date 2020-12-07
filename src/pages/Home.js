5ca45da61063acb9347e8ae48d309186d7fa9785
kpegeder <k.pegeder@gmail.com>
2020-12-06 21:49:04 -0800

    update component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,18 @@
 import React from "react";
 import Header from "../components/Header";
 import About from "../components/About";
-import Knowledge from "../components/Knowledge";
+import Skills from "../components/Skills";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
     <div>
       <Header />
       <About />
-      <Knowledge />
+      <Skills />
+      <Contact />
+      <Footer />
     </div>
   );
 };
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Projects from "../components/Projects";
 
 const Portfolio = () => {
   return (
     <div>
       <Header />
+      <Projects />
+      <Footer />
     </div>
   );
 };
