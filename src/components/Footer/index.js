f7e0e1f9beea0775d4526cfea299446ce2a00d82
kpegeder <k.pegeder@gmail.com>
2020-12-06 21:45:21 -0800

    add footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <div>
+      <div className="footer">
+        <h1>Footer</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
