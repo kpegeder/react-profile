2899347aba93b8ac29435b054ec5b1b49692b849
kpegeder <k.pegeder@gmail.com>
2020-12-06 21:43:44 -0800

    add contact component

---
+++ src/components/Contact/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+
+function Contact() {
+  return (
+    <div>
+      <div className="contact-section">
+        <div className="contact-me">
+          <h1>Contact</h1>
+          <h2>I'm a developer.</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
