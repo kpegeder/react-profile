7d7695b3491af46145dba99dfca06fc0297a472c
kpegeder <k.pegeder@gmail.com>
2020-12-06 18:49:59 -0800

    add skills component

---
+++ src/components/Knowledge/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+
+function Knowledge() {
+  return (
+    <div>
+      <div className="knowledge-section">
+        <div className="skills">
+          <h1>Knowledge </h1>
+          <h2>I'm a developer.</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Knowledge;
