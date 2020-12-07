75843f9200540a5f2e504d146803cfca42a3cd76
kpegeder <k.pegeder@gmail.com>
2020-12-06 21:47:08 -0800

    add projects component

---
+++ src/components/Projects/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+const Projects = () => {
+  return (
+    <div>
+      <div className="card">Project 1</div>
+    </div>
+  );
+};
+
+export default Projects;
