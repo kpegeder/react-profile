a393c0129bae8a7498565ad9efc3e322c27ce64d
kpegeder <k.pegeder@gmail.com>
2020-12-06 18:49:36 -0800

    add about component

---
+++ src/components/About/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+
+function About() {
+  return (
+    <div>
+      <div className="about-section">
+        <div className="name">
+          <h1>About Me</h1>
+          <h2>I'm a developer.</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
