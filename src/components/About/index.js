a1b21f0fdf5a645e9cb2e5d0d7d8c0d42bb028af
kpegeder <k.pegeder@gmail.com>
2020-12-06 22:07:54 -0800

    add paragraph in about me

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,7 +7,24 @@ function About() {
       <div className="about-section">
         <div className="about">
           <h1>About Me</h1>
-          <h2>I'm a developer.</h2>
+          <p className="about-me">
+            Growing up, my favorite subject in school had to have been math. For
+            me, math was logical with a right or wrong answer. As I started to
+            take harder math classes, I viewed math as a puzzle. I enjoy seeing
+            a problem and thinking about ways to fix them. It wasn’t until I was
+            in college at Oregon State University that I learned coding. While I
+            was in college, I always enjoyed when we had problems which required
+            coding. Since graduation, I have found a passion for helping others
+            learn or improve their knowledge. I have worked as a ski instructor
+            predominating working with young children (3.5-5 yrs.), coached
+            soccer to kids (8-10 yrs.), and tutor math at a community college.
+            Now, it’s time for me to reset and think about what I like doing. It
+            has led me to self-teaching myself how to coding a different
+            language than college. In my spare time, I enjoy doing outdoor
+            activities such as soccer, ultimate frisbee, skiing, and hiking.
+            When I’m not outdoors, you’ll find me learning coding and listening
+            to music.
+          </p>
         </div>
       </div>
     </div>
