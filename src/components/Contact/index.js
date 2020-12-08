1a4bb7833965b4cf081a1c7c1e40383e79664380
kpegeder <k.pegeder@gmail.com>
2020-12-07 21:28:32 -0800

    add contact info

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -3,12 +3,41 @@ import "./style.css";
 
 function Contact() {
   return (
-    <div>
-      <div className="contact-section">
-        <div className="contact-me">
-          <h1>Contact</h1>
-          <h2>I'm a developer.</h2>
-        </div>
+    <div className="row justify-content-md-center">
+      <div className="center col-md-12">
+        <h2>Contact</h2>
+      </div>
+      <div className="job-icon col-md-4">
+        <i className="fab fa-linkedin">
+          LinkedIn Profile:
+          <a
+            href="https://www.linkedin.com/in/kalenpegeder/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            LinkedIn
+          </a>
+        </i>
+      </div>
+      <div className="job-icon col-md-4">
+        <i className="far fa-envelope">
+          Email:
+          <a href="mailto:k.pegeder@gmail.com" target="_blank" rel="noreferrer">
+            k.pegeder@gmail.com
+          </a>
+        </i>
+      </div>
+      <div className="job-icon col-md-4">
+        <i className="fab fa-github">
+          GitHub:
+          <a
+            href="https://github.com/kpegeder"
+            target="_blank"
+            rel="noreferrer"
+          >
+            github.com/kpegeder
+          </a>
+        </i>
       </div>
     </div>
   );
