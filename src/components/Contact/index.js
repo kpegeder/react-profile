a8c95c597bea3824302d9c499a18c2f310472d0d
kpegeder <k.pegeder@gmail.com>
2021-01-04 16:10:25 -0800

    make contact mobile friendly

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,44 +1,56 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 import "./style.css";
 
 function Contact() {
   return (
-    <div className="row justify-content-md-center">
-      <div className="center col-md-12">
-        <h2>Contact</h2>
-      </div>
-      <div className="job-icon col-md-4">
-        <i className="fab fa-linkedin">
-          LinkedIn Profile:
-          <a
-            href="https://www.linkedin.com/in/kalenpegeder/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            LinkedIn
-          </a>
-        </i>
-      </div>
-      <div className="job-icon col-md-4">
-        <i className="far fa-envelope">
-          Email:
-          <a href="mailto:k.pegeder@gmail.com" target="_blank" rel="noreferrer">
-            k.pegeder@gmail.com
-          </a>
-        </i>
-      </div>
-      <div className="job-icon col-md-4">
-        <i className="fab fa-github">
-          GitHub:
-          <a
-            href="https://github.com/kpegeder"
-            target="_blank"
-            rel="noreferrer"
-          >
-            github.com/kpegeder
-          </a>
-        </i>
-      </div>
+    <div className="contact-section">
+      <Container>
+        <Row>
+          <Col className="contactHeader">Contact</Col>
+        </Row>
+        <Row>
+          <Col sm={4} className="job-icon contact-me">
+            <i className="fab fa-linkedin">
+              {" "}
+              LinkedIn Profile:{" "}
+              <a
+                href="https://www.linkedin.com/in/kalenpegeder/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                LinkedIn
+              </a>
+            </i>
+          </Col>
+          <Col sm={4} className="job-icon contact-me">
+            <i className="far fa-envelope">
+              {" "}
+              Email:{" "}
+              <a
+                href="mailto:k.pegeder@gmail.com"
+                target="_blank"
+                rel="noreferrer"
+              >
+                k.pegeder@gmail.com
+              </a>
+            </i>
+          </Col>
+          <Col sm={4} className="job-icon contact-me">
+            <i className="fab fa-github">
+              {" "}
+              GitHub:{" "}
+              <a
+                href="https://github.com/kpegeder"
+                target="_blank"
+                rel="noreferrer"
+              >
+                github.com/kpegeder
+              </a>
+            </i>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
